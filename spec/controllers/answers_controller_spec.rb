f5dfcdd49b28c5e7039df3dd2f9d6d411e076339
hunter298 <anton-volianskyi@ukr.net>
2020-06-06 22:49:10 +0300

    invalid attributes for answer tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   describe 'GET #new' do
-    before { get :new, params: { question_id: create(:question) } }
+    before { get :new, params: {question_id: create(:question)} }
 
     it 'renders new view' do
       expect(response).to render_template :new
@@ -10,14 +10,28 @@ RSpec.describe AnswersController, type: :controller do
   end
 
   describe 'POST #create' do
-    it 'save new answer to database' do
-      expect { post :create, params: { question_id: create(:question), answer: attributes_for(:answer) }}.to change(Answer, :count).by(1)
+    context 'with valid attributes' do
+      it 'save new answer to database' do
+        expect { post :create, params: {question_id: create(:question), answer: attributes_for(:answer)} }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirect to question' do
+        post :create, params: {question_id: create(:question), answer: attributes_for(:answer)}
+
+        expect(response).to redirect_to (assigns(:question))
+      end
     end
 
-    it 'redirect to question' do
-      post :create, params: { question_id: create(:question), answer: attributes_for(:answer) }
+    context 'with invalid attributes' do
+      it 'does not save answer to database' do
+        expect { post :create, params: { question_id: create(:question), answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+      end
 
-      expect(response).to redirect_to (assigns(:question))
+      it 'renders new view' do
+        post :create, params: { question_id: create(:question), answer: attributes_for(:answer, :invalid) }
+
+        expect(response).to render_template(:new)
+      end
     end
   end
 
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :answer do
     body { "MyText" }
     question { nil }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
