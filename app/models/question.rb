b9c477b70a54aa2f7b214f20e5b408ea9d26525f
hunter298 <anton-volianskyi@ukr.net>
2020-06-27 23:37:51 +0300

    cocoon for nested forms

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,9 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  has_many :links, dependent: :destroy, as: :linkable
+  has_many :links, dependent: :destroy, as: :linkable, inverse_of: :linkable
   belongs_to :user
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank
+  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
   has_many_attached :files
 
