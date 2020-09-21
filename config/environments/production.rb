172122735ac6437d276b9cd5570e1cabc5fffa7e
hunter298 <anton-volianskyi@ukr.net>
2020-09-22 01:52:18 +0300

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "qna_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'http://207.154.223.29/' }
+  config.action_mailer.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
